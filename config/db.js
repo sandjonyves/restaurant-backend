7a83e11941c88a71bef514a78aecace16278d4ee
sandjonyves <sandjonyves@gmail.com>
2025-09-25 00:38:48 +0200

    url database add

--- config/db.js
+++ config/db.js
@@ -1,9 +1,32 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './database.sqlite', // fichier SQLite à la racine du projet
-  logging: false,
-});
+const env = process.env.NODE_ENV || 'dev';
+
+let sequelize;
+
+if (env === 'dev' || env === 'prod') {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: 'postgres',
+    logging: false,
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    },
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  });
+} else {
+  sequelize = new Sequelize({
+    dialect: 'sqlite',
+    storage: './database.sqlite',
+    logging: false
+  });
+}
 
 module.exports = sequelize;
